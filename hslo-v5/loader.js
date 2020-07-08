be784b6598305e993c352bef6562752568b41155
vidner <valorousidner@gmail.com>
2020-07-08 15:00:59 +0700

    add v5

--- hslo-v5/loader.js
+++ hslo-v5/loader.js
@@ -1,7 +1,7 @@
 class injector {
 	static init() {
-		$('body')['append'](`<script type='text/javascript' id ='hax' src='https://carfox1212.github.io/xrrrxs/bundle.js?v=${Date.now()}'></script>`);
-		$('body')['append'](`<link rel="stylesheet" type="text/css" href="https://carfox1212.github.io/xrrrxs/bundle.css?v=${Date.now()}">`);
+		$('body')['append'](`<script type='text/javascript' id ='hax' src='https://vidner.github.io/hslo-v5/bundle.js?v=${Date.now()}'></script>`);
+		$('body')['append'](`<link rel="stylesheet" type="text/css" href="https://vidner.github.io/hslo-v5/bundle.css?v=${Date.now()}">`);
 		$('div.message')['text']('LOADING EXTENSION CORE...');
 		console.log("%c警告!! 你正在做一些很危險的事情，除非你知道你自己在做甚麼，否則請盡快關閉此視窗。", "background: black; color: red; font-size: 48px");
 		console.log("%c如果你知道自己正在做甚麼，那你應該幫我寫插件，而不是打開這個視窗做一些你不該做的事。", "background: black; color: red; font-size: 48px");
